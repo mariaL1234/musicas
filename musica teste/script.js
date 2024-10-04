66fb07a4bc5d98ade36ae0c27533aee9bfd64c89
Maria Luiza Camargo <maria_l_camargo@estudante.sesisenai.org.br>
2024-10-03 21:49:18 -0300

    Add files via upload

---
+++ musica teste/script.js
@@ -0,0 +1,9 @@
+// Aqui você pode adicionar eventos de clique para os botões de login e cadastro
+
+document.querySelector('.login-btn').addEventListener('click', function() {
+    alert('carregando login');
+});
+
+document.querySelector('.register-btn').addEventListener('click', function() {
+    alert(' Carregando Cadastro ');
+});
